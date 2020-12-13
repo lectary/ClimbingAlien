5d052f757abd2c3a55b3d3259a11559cf16b098f
Günter Windsperger <e1302775@student.tuwien.ac.at>
2020-12-13 17:17:42 +0100

    A new climax is born
    - Adding utils class for degrees to radians conversion
    - Extending painter class to draw a climax like obstacle

---
+++ lib/utils/utils.dart
@@ -0,0 +1,8 @@
+import 'dart:math';
+
+class Utils {
+  /// Helper function for converting degress to radians
+  static num degreesToRadians(num deg) {
+    return (deg * pi) / 180.0;
+  }
+}
--- lib/views/climbing_alien_painter.dart
+++ lib/views/climbing_alien_painter.dart
@@ -1,5 +1,9 @@
+import 'dart:math';
+
+import 'package:climbing_alien/utils/utils.dart';
 import 'package:flutter/material.dart';
 
+/// Class for drawing Climax, the Cli-mbing max-erl.
 class ClimbingAlienPainter extends CustomPainter {
   final Offset position;
 
@@ -9,9 +13,37 @@ class ClimbingAlienPainter extends CustomPainter {
   void paint(Canvas canvas, Size size) {
     var paint = Paint();
     paint.color = Colors.amber;
-    paint.strokeWidth = 5;
 
-    canvas.drawCircle(position, size.height / 16, paint);
+    // body
+    var bodyRect = Rect.fromCenter(center: position, width: 50, height: 80);
+    canvas.drawOval(bodyRect, paint);
+
+    // limbs
+    var circleBottomLeftCenter = position + Offset(-50, 70);
+    var circleBottomRightCenter = position + Offset(50, 70);
+    var circleTopLeftCenter = position + Offset(-50, -70);
+    var circleTopRightCenter = position + Offset(50, -70);
+    var radius = 20.0;
+
+    paint.style = PaintingStyle.stroke; // obstacles are not filled
+    paint.strokeWidth = 3.0;
+    canvas.drawCircle(circleBottomLeftCenter, radius, paint);
+    canvas.drawCircle(circleBottomRightCenter, radius, paint);
+    canvas.drawCircle(circleTopLeftCenter, radius, paint);
+    canvas.drawCircle(circleTopRightCenter, radius, paint);
+
+    // arms
+    Offset offsetForPointOnBorderBottomLeft = Offset(cos(Utils.degreesToRadians(45)) * radius, -cos(Utils.degreesToRadians(45)) * radius);
+    canvas.drawLine(circleBottomLeftCenter + offsetForPointOnBorderBottomLeft, position, paint);
+
+    Offset offsetForPointOnBorderBottomRight = Offset(-cos(Utils.degreesToRadians(45)) * radius, -cos(Utils.degreesToRadians(45)) * radius);
+    canvas.drawLine(circleBottomRightCenter + offsetForPointOnBorderBottomRight, position, paint);
+
+    Offset offsetForPointOnBorderTopLeft = Offset(cos(Utils.degreesToRadians(45)) * radius, cos(Utils.degreesToRadians(45)) * radius);
+    canvas.drawLine(circleTopLeftCenter + offsetForPointOnBorderTopLeft, position, paint);
+
+    Offset offsetForPointOnBorderTopRight = Offset(-cos(Utils.degreesToRadians(45)) * radius, cos(Utils.degreesToRadians(45)) * radius);
+    canvas.drawLine(circleTopRightCenter + offsetForPointOnBorderTopRight, position, paint);
   }
 
   @override
