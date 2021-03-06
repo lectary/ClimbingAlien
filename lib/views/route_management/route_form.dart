50947aa5c984aaa6af5f1339cd11d535e61c45d8
Günter Windsperger <e1302775@student.tuwien.ac.at>
2021-03-06 14:41:12 +0100

    Add route screen and form

---
+++ lib/views/route_management/route_form.dart
@@ -0,0 +1,110 @@
+import 'package:climbing_alien/data/entity/route.dart';
+import 'package:climbing_alien/viewmodels/route_viewmodel.dart';
+import 'package:flutter/material.dart' hide Route;
+import 'package:provider/provider.dart';
+
+class RouteForm extends StatefulWidget {
+  final Route route;
+  final int wallId;
+
+  RouteForm(this.route, this.wallId);
+
+  @override
+  _RouteFormState createState() => _RouteFormState();
+
+  static Future<bool> showRouteFormDialog(BuildContext context, {Route route, int wallId}) async {
+    assert(route != null ? wallId != null : true);
+    final model = Provider.of<RouteViewModel>(context, listen: false);
+    return await showDialog<bool>(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) => ChangeNotifierProvider.value(
+              value: model,
+              child: AlertDialog(
+                title: route == null ? Text("Neue Route") : Text("Bearbeite Route"),
+                content: RouteForm(route, wallId),
+              ),
+            ));
+  }
+}
+
+class _RouteFormState extends State<RouteForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final FocusNode _focusNode = FocusNode();
+  RouteViewModel routeViewModel;
+  bool edit;
+
+  String title;
+  String description;
+
+  @override
+  void initState() {
+    super.initState();
+    routeViewModel = Provider.of<RouteViewModel>(context, listen: false);
+    _focusNode.requestFocus();
+    edit = widget.route != null;
+    title = widget.route?.title;
+    description = widget.route?.description;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          TextFormField(
+            initialValue: title,
+            focusNode: _focusNode,
+            decoration: InputDecoration(hintText: "Titel"),
+            validator: (value) {
+              if (value.isEmpty) {
+                return "Titel wird benötigt!";
+              }
+              return null;
+            },
+            onSaved: (value) => title = value,
+          ),
+          TextFormField(
+            initialValue: description,
+            decoration: InputDecoration(hintText: "Beschreibung"),
+            onSaved: (value) => description = value,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 32.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ElevatedButton(
+                    child: Text("Abbrechen"),
+                    style: ElevatedButton.styleFrom(primary: Theme.of(context).colorScheme.error),
+                    onPressed: () => Navigator.pop(context)),
+                ElevatedButton(
+                    child: Text("Speichern"),
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        _formKey.currentState.save();
+                        if (edit) {
+                          widget.route.title = title;
+                          widget.route.description = description;
+                          routeViewModel.updateRoute(widget.route);
+                        } else {
+                          Route route = Route(
+                            title,
+                            widget.wallId,
+                            description: description,
+                          );
+                          routeViewModel.insertRoute(route);
+                        }
+                        Navigator.pop(context);
+                      }
+                    })
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/views/route_management/route_screen.dart
@@ -0,0 +1,52 @@
+import 'package:climbing_alien/data/entity/route.dart';
+import 'package:climbing_alien/data/entity/wall.dart';
+import 'package:climbing_alien/viewmodels/route_viewmodel.dart';
+import 'package:climbing_alien/views/route_editor/route_editor_screen.dart';
+import 'package:climbing_alien/views/route_management/route_form.dart';
+import 'package:flutter/material.dart' hide Route;
+import 'package:provider/provider.dart';
+
+class RouteScreen extends StatelessWidget {
+  final Wall wall;
+
+  RouteScreen(this.wall);
+
+  @override
+  Widget build(BuildContext context) {
+    final routeModel = Provider.of<RouteViewModel>(context, listen: false);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("${wall.title} - Routen"),
+      ),
+      body: StreamBuilder<List<Route>>(
+        stream: routeModel.routeStream,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final routeList = snapshot.data;
+            return routeList.isEmpty
+                ? Center(child: Text("Keine Routen vorhanden"))
+                : ListView.builder(
+                    itemCount: routeList.length,
+                    itemBuilder: (context, index) {
+                      final route = routeList[index];
+                      return ListTile(
+                        title: Text(route.title),
+                        subtitle: Text(route.description),
+                        trailing: IconButton(
+                            icon: Icon(Icons.edit),
+                            onPressed: () => Navigator.pushNamed(context, RouteEditorScreen.routeName)),
+                        onLongPress: () => RouteForm.showRouteFormDialog(context, route: route, wallId: wall.id),
+                      );
+                    });
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => RouteForm.showRouteFormDialog(context, wallId: wall.id),
+      ),
+    );
+  }
+}
