5f7d34d3c42ab4e69f6c4491311398e4c84fe59a
Günter Windsperger <e1302775@student.tuwien.ac.at>
2021-03-07 06:06:45 +0100

    Add functions to persist wall images on device

--- lib/viewmodels/wall_viewmodel.dart
+++ lib/viewmodels/wall_viewmodel.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:climbing_alien/data/climbing_repository.dart';
 import 'package:climbing_alien/data/entity/wall.dart';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 
 class WallViewModel extends ChangeNotifier {
   final ClimbingRepository _climbingRepository;
@@ -11,18 +15,44 @@ class WallViewModel extends ChangeNotifier {
 
   Stream<List<Wall>> get wallStream => _climbingRepository.watchAllWalls();
 
-  Future<void> insertWall(Wall wall) {
+  Future<void> insertWall(Wall wall) async {
+    if (wall.imagePath != null && !wall.imagePath.startsWith('assets')) {
+      String newPath = await _saveImageToDevice(wall.imagePath);
+      wall.imagePath = newPath;
+    }
     return _climbingRepository.insertWall(wall);
   }
 
-  Future<void> updateWall(Wall wall) {
+  Future<void> updateWall(Wall wall) async {
     if (wall.imagePath != wall.imagePathUpdated) {
       wall.imagePath = wall.imagePathUpdated;
+      if (!wall.imagePath.startsWith('assets')) {
+        String newPath = await _saveImageToDevice(wall.imagePath);
+        wall.imagePath = newPath;
+      }
     }
     return _climbingRepository.updateWall(wall);
   }
 
   Future<void> deleteWall(Wall wall) {
+    if (!wall.imagePath.startsWith('assets')) {
+      _deleteImageFromDevice(wall.imagePath);
+    }
     return _climbingRepository.deleteWall(wall);
+  }
+
+  Future<String> _saveImageToDevice(String imagePath) async {
+    if (imagePath.contains('assets')) return null;
+    File tmpFile = File(imagePath);
+    final String filename = basename(imagePath); // Filename without extension
+
+    final directory = await getApplicationDocumentsDirectory();
+    String filePath = '${directory.path}/$filename';
+    await tmpFile.copy(filePath);
+    return filePath;
+  }
+
+  Future<void> _deleteImageFromDevice(String imagePath) async {
+    await File(imagePath).delete();
   }
 }
