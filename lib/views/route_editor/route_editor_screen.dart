aee9dd8f36d693015e3af35d533a0e921b9db763
Günter Windsperger <e1302775@student.tuwien.ac.at>
2021-03-21 08:58:26 +0100

    Move joystick to top right of the screen

--- lib/views/route_editor/route_editor_screen.dart
+++ lib/views/route_editor/route_editor_screen.dart
@@ -169,10 +169,11 @@ class RouteEditorScreen extends StatelessWidget {
       final climaxModel = Provider.of<ClimaxViewModel>(context, listen: false);
       return Positioned(
           right: 0,
-          bottom: 0,
+          top: 0,
           child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32.0),
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16.0),
             child: JoystickWithButtonAndSlider(
+              sliderSide: SliderSide.LEFT,
               onDirectionChanged: (degrees, distance) {
                 climaxModel.moveLimbFree(degrees, distance);
               },
