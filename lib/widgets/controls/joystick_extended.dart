29767a58fd4229d120bd34541c6b784e16a11b1c
Günter Windsperger <e1302775@student.tuwien.ac.at>
2021-03-21 08:57:23 +0100

    Update joystick with the possibility to place the slider on different sides

--- lib/widgets/controls/joystick_extended.dart
+++ lib/widgets/controls/joystick_extended.dart
@@ -10,6 +10,10 @@ typedef JoystickSliderCallback = void Function(double speed);
 const defaultSize = 100.0;
 const defaultStickSize = 50.0;
 
+/// Specifies on which side of the joystick the slider will be displayed
+enum SliderSide { LEFT, RIGHT, TOP, BOTTOM }
+
+/// A joystick with directional buttons and a slider for adjusting speed.
 class JoystickWithButtonAndSlider extends StatefulWidget {
   final double size;
   final double sizeControlStick;
@@ -18,6 +22,7 @@ class JoystickWithButtonAndSlider extends StatefulWidget {
   final Color colorBackground;
   final Color colorControlStick;
   final Color colorIcon;
+  final SliderSide sliderSide;
 
   final Function onClickedUp;
   final Function onClickedDown;
@@ -32,6 +37,7 @@ class JoystickWithButtonAndSlider extends StatefulWidget {
       this.colorBackground = Colors.grey,
       this.colorControlStick = Colors.blueGrey,
       this.colorIcon = Colors.white,
+      this.sliderSide = SliderSide.BOTTOM,
       this.onClickedUp,
       this.onClickedDown,
       this.onClickedLeft,
@@ -69,7 +75,8 @@ class _JoystickWithButtonAndSliderState extends State<JoystickWithButtonAndSlide
     double bigRadius = outerSize / 2.0;
 
     Offset shiftedOffset = (offset - center); // Shift to get the offset with the center as origin.
-    double angle = _math.atan2(shiftedOffset.dy, shiftedOffset.dx); // Calculates the angle in radians between x-axis and the point.
+    double angle = _math.atan2(
+        shiftedOffset.dy, shiftedOffset.dx); // Calculates the angle in radians between x-axis and the point.
     // Correcting degrees interval to [0, 360]. The interval starts analogues to a clock at 12 and runs clockwise.
     double degrees = (angle * 180 / _math.pi + 90);
     if (offset.dx < bigRadius && offset.dy < bigRadius) {
@@ -92,10 +99,12 @@ class _JoystickWithButtonAndSliderState extends State<JoystickWithButtonAndSlide
     double smallRadius = controlStickSize / 2.0;
     double bigRadius = outerSize / 2.0;
 
-    double maxDistance = outerSize / 2.0 - smallRadius; // Max. distance the stick should go, i.e. till it touches outer circle.
+    double maxDistance =
+        outerSize / 2.0 - smallRadius; // Max. distance the stick should go, i.e. till it touches outer circle.
 
     Offset shiftedOffset = (offset - center); // Shift to get the offset with the center as origin.
-    double angle = _math.atan2(shiftedOffset.dy, shiftedOffset.dx); // Calculates the angle in radians between x-axis and the point.
+    double angle = _math.atan2(
+        shiftedOffset.dy, shiftedOffset.dx); // Calculates the angle in radians between x-axis and the point.
     double degrees = (angle * 180 / _math.pi); // Convert to degrees, results are in interval [-180, 180].
     // Correcting degrees interval to [0, 360].
     if (offset.dy < bigRadius) {
@@ -127,84 +136,110 @@ class _JoystickWithButtonAndSliderState extends State<JoystickWithButtonAndSlide
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          width: outerSize,
-          height: outerSize,
-          child: Material(
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(50.0),
-                side: BorderSide(width: 2.0, color: widget.colorControlStick)),
-            color: widget.colorBackground,
-            child: GestureDetector(
-              behavior: HitTestBehavior.deferToChild,
-              onPanUpdate: (details) => setState(() {
-                controlStickPosition = calculateControlStickPosition(details.localPosition);
-                processCallback(details.localPosition);
-              }),
-              onPanEnd: (details) => setState(() {
-                controlStickPosition = calculateControlStickPosition(center);
-                processCallback(center);
-              }),
-              child: Stack(
+    return _buildJoystickWithSliderBasedOnSliderSide(widget.sliderSide);
+  }
+
+  _buildJoystickWithSliderBasedOnSliderSide(SliderSide sliderSide) {
+    switch (sliderSide) {
+      case SliderSide.LEFT:
+        return Row(children: [
+          RotatedBox(quarterTurns: 3, child: _buildSlider()),
+          _buildJoystick(),
+        ]);
+      case SliderSide.RIGHT:
+        return Row(children: [
+          _buildJoystick(),
+          RotatedBox(quarterTurns: 1, child: _buildSlider()),
+        ]);
+      case SliderSide.TOP:
+        return Column(children: [
+          _buildSlider(),
+          _buildJoystick(),
+        ]);
+      case SliderSide.BOTTOM:
+        return Column(children: [
+          _buildJoystick(),
+          _buildSlider(),
+        ]);
+    }
+  }
+
+  Widget _buildSlider() {
+    return CustomSliderWithLabel(widget.size, maxSpeed, speed, (double value) {
+      if (widget.onSliderChanged != null) {
+        widget.onSliderChanged(value);
+      }
+      setState(() {
+        speed = value;
+      });
+    });
+  }
+
+  Widget _buildJoystick() {
+    return Container(
+      width: outerSize,
+      height: outerSize,
+      child: Material(
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(50.0), side: BorderSide(width: 2.0, color: widget.colorControlStick)),
+        color: widget.colorBackground,
+        child: GestureDetector(
+          behavior: HitTestBehavior.deferToChild,
+          onPanUpdate: (details) => setState(() {
+            controlStickPosition = calculateControlStickPosition(details.localPosition);
+            processCallback(details.localPosition);
+          }),
+          onPanEnd: (details) => setState(() {
+            controlStickPosition = calculateControlStickPosition(center);
+            processCallback(center);
+          }),
+          child: Stack(
+            children: [
+              Stack(
                 children: [
-                  Stack(
-                    children: [
-                      Align(
-                          alignment: Alignment.topCenter,
-                          child: InkResponse(
-                              radius: widget.sizeControlStick / 4,
-                              splashColor: widget.colorControlStick,
-                              onTap: widget.onClickedUp ?? () {},
-                              child: Icon(Icons.keyboard_arrow_up, color: widget.colorIcon))),
-                      Align(
-                          alignment: Alignment.bottomCenter,
-                          child: InkResponse(
-                              radius: widget.sizeControlStick / 4,
-                              splashColor: widget.colorControlStick,
-                              onTap: widget.onClickedDown ?? () {},
-                              child: Icon(Icons.keyboard_arrow_down, color: widget.colorIcon))),
-                      Align(
-                          alignment: Alignment.centerLeft,
-                          child: InkResponse(
-                              radius: widget.sizeControlStick / 4,
-                              splashColor: widget.colorControlStick,
-                              onTap: widget.onClickedLeft ?? () {},
-                              child: Icon(Icons.keyboard_arrow_left, color: widget.colorIcon))),
-                      Align(
-                          alignment: Alignment.centerRight,
-                          child: InkResponse(
-                              radius: widget.sizeControlStick / 4,
-                              splashColor: widget.colorControlStick,
-                              onTap: widget.onClickedRight ?? () {},
-                              child: Icon(Icons.keyboard_arrow_right, color: widget.colorIcon))),
-                    ],
-                  ),
-                  Positioned(
-                    top: controlStickPosition.dy,
-                    left: controlStickPosition.dx,
-                    child: Container(
-                      width: controlStickSize,
-                      height: controlStickSize,
-                      decoration: BoxDecoration(color: widget.colorControlStick, shape: BoxShape.circle),
-                    ),
-                  ),
+                  Align(
+                      alignment: Alignment.topCenter,
+                      child: InkResponse(
+                          radius: widget.sizeControlStick / 4,
+                          splashColor: widget.colorControlStick,
+                          onTap: widget.onClickedUp ?? () {},
+                          child: Icon(Icons.keyboard_arrow_up, color: widget.colorIcon))),
+                  Align(
+                      alignment: Alignment.bottomCenter,
+                      child: InkResponse(
+                          radius: widget.sizeControlStick / 4,
+                          splashColor: widget.colorControlStick,
+                          onTap: widget.onClickedDown ?? () {},
+                          child: Icon(Icons.keyboard_arrow_down, color: widget.colorIcon))),
+                  Align(
+                      alignment: Alignment.centerLeft,
+                      child: InkResponse(
+                          radius: widget.sizeControlStick / 4,
+                          splashColor: widget.colorControlStick,
+                          onTap: widget.onClickedLeft ?? () {},
+                          child: Icon(Icons.keyboard_arrow_left, color: widget.colorIcon))),
+                  Align(
+                      alignment: Alignment.centerRight,
+                      child: InkResponse(
+                          radius: widget.sizeControlStick / 4,
+                          splashColor: widget.colorControlStick,
+                          onTap: widget.onClickedRight ?? () {},
+                          child: Icon(Icons.keyboard_arrow_right, color: widget.colorIcon))),
                 ],
               ),
-            ),
+              Positioned(
+                top: controlStickPosition.dy,
+                left: controlStickPosition.dx,
+                child: Container(
+                  width: controlStickSize,
+                  height: controlStickSize,
+                  decoration: BoxDecoration(color: widget.colorControlStick, shape: BoxShape.circle),
+                ),
+              ),
+            ],
           ),
         ),
-        CustomSliderWithLabel(widget.size, maxSpeed, speed, (double value) {
-          if (widget.onSliderChanged != null) {
-            widget.onSliderChanged(value);
-          }
-          setState(() {
-            speed = value;
-          });
-        }),
-      ],
+      ),
     );
   }
 }
-
