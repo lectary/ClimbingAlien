4aeb41aed9fca43558d09f9c2c4dfefcd54f83a2
Günter Windsperger <e1302775@student.tuwien.ac.at>
2021-03-27 15:32:23 +0100

    Add routeOption to routeViewer

--- lib/viewmodels/route_viewmodel.dart
+++ lib/viewmodels/route_viewmodel.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:climbing_alien/data/climbing_repository.dart';
 import 'package:climbing_alien/data/entity/grasp.dart';
 import 'package:climbing_alien/data/entity/route.dart';
+import 'package:climbing_alien/data/entity/route_option.dart';
 import 'package:flutter/foundation.dart';
 
 class RouteViewModel extends ChangeNotifier {
@@ -27,4 +28,11 @@ class RouteViewModel extends ChangeNotifier {
 
   /// Grasps
   Stream<List<Grasp>> getGraspStreamByRouteId(int routeId) => _climbingRepository.watchAllGraspsByRouteId(routeId);
+
+  Future<RouteOption?> getRouteOption(Route route) {
+    if (route.routeOptionId == null) {
+      return Future.value(null);
+    }
+    return _climbingRepository.findRouteOptionById(route.routeOptionId!);
+  }
 }
--- lib/views/route_viewer/route_viewer_screen.dart
+++ lib/views/route_viewer/route_viewer_screen.dart
@@ -1,6 +1,7 @@
 import 'package:climbing_alien/data/climbing_repository.dart';
 import 'package:climbing_alien/data/entity/grasp.dart';
 import 'package:climbing_alien/data/entity/route.dart';
+import 'package:climbing_alien/data/entity/route_option.dart';
 import 'package:climbing_alien/data/entity/wall.dart';
 import 'package:climbing_alien/viewmodels/climax_viewmodel.dart';
 import 'package:climbing_alien/viewmodels/route_viewmodel.dart';
@@ -44,73 +45,87 @@ class _RouteViewerScreenState extends State<RouteViewerScreen> {
               appBar: AppBar(
                 title: Text('Route Viewer'),
               ),
-              body: StreamBuilder<List<Grasp>>(
-                  stream: routeModel.getGraspStreamByRouteId(widget.route.id!),
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      final _graspList = snapshot.data!;
-                      if (_graspList.isEmpty) {
-                        return Center(child: Text('No grasps available'));
-                      } else {
-                        _graspList.sort((g1, g2) => g1.order! - g2.order!);
-                        climaxModel.setupByGrasp(_graspList[index]);
-                        return Column(
-                          children: [
-                            Expanded(
-                              child: Stack(
-                                children: [
-                                  Positioned.fill(
-                                    child: ClimaxTransformer(imagePath: widget.wall.imagePath),
-                                  ),
-                                  Positioned(
-                                      left: 0,
-                                      right: 0,
-                                      bottom: 0,
-                                      child: ButtonBar(
-                                        alignment: MainAxisAlignment.spaceAround,
-                                        children: [
-                                          ElevatedButton(
-                                              onPressed: (index > 0)
-                                                  ? () {
-                                                      // Double check to avoid invalid executions due button hammering and execution-lag
-                                                      if (index <= 0) return;
-                                                      setState(() {
-                                                        index--;
-                                                      });
-                                                    }
-                                                  : null,
-                                              child: Text("-")),
-                                          TextButton(
-                                            style: TextButton.styleFrom(
-                                              backgroundColor: Theme.of(context).colorScheme.primary,
-                                            ),
-                                            onPressed: () {},
-                                            child: Text('Grasp ${index + 1} of ${_graspList.length}',
-                                                style: TextStyle(color: Theme.of(context).colorScheme.onPrimary)),
-                                          ),
-                                          ElevatedButton(
-                                              onPressed: (index < _graspList.length - 1)
-                                                  ? () {
-                                                      // Double check to avoid invalid executions due button hammering and execution-lag
-                                                      if (index >= _graspList.length - 1) return;
-                                                      setState(() {
-                                                        index++;
-                                                      });
-                                                    }
-                                                  : null,
-                                              child: Text("+"))
-                                        ],
-                                      ))
-                                ],
-                              ),
-                            ),
-                          ],
-                        );
-                      }
-                    } else {
-                      return Center(child: CircularProgressIndicator());
+              body: FutureBuilder<RouteOption?>(
+                future: routeModel.getRouteOption(widget.route),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    if (snapshot.data != null) {
+                      final RouteOption routeOption = snapshot.data!;
+                      climaxModel.scaleBackground = routeOption.scaleBackground;
+                      climaxModel.deltaTranslateBackground = routeOption.translateBackground;
                     }
-                  }),
+                    return StreamBuilder<List<Grasp>>(
+                        stream: routeModel.getGraspStreamByRouteId(widget.route.id!),
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                            final _graspList = snapshot.data!;
+                            if (_graspList.isEmpty) {
+                              return Center(child: Text('No grasps available'));
+                            } else {
+                              _graspList.sort((g1, g2) => g1.order! - g2.order!);
+                              climaxModel.setupByGrasp(_graspList[index]);
+                              return Column(
+                                children: [
+                                  Expanded(
+                                    child: Stack(
+                                      children: [
+                                        Positioned.fill(
+                                          child: ClimaxTransformer(imagePath: widget.wall.imagePath),
+                                        ),
+                                        Positioned(
+                                            left: 0,
+                                            right: 0,
+                                            bottom: 0,
+                                            child: ButtonBar(
+                                              alignment: MainAxisAlignment.spaceAround,
+                                              children: [
+                                                ElevatedButton(
+                                                    onPressed: (index > 0)
+                                                        ? () {
+                                                            // Double check to avoid invalid executions due button hammering and execution-lag
+                                                            if (index <= 0) return;
+                                                            setState(() {
+                                                              index--;
+                                                            });
+                                                          }
+                                                        : null,
+                                                    child: Text("-")),
+                                                TextButton(
+                                                  style: TextButton.styleFrom(
+                                                    backgroundColor: Theme.of(context).colorScheme.primary,
+                                                  ),
+                                                  onPressed: () {},
+                                                  child: Text('Grasp ${index + 1} of ${_graspList.length}',
+                                                      style: TextStyle(color: Theme.of(context).colorScheme.onPrimary)),
+                                                ),
+                                                ElevatedButton(
+                                                    onPressed: (index < _graspList.length - 1)
+                                                        ? () {
+                                                            // Double check to avoid invalid executions due button hammering and execution-lag
+                                                            if (index >= _graspList.length - 1) return;
+                                                            setState(() {
+                                                              index++;
+                                                            });
+                                                          }
+                                                        : null,
+                                                    child: Text("+"))
+                                              ],
+                                            ))
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              );
+                            }
+                          } else {
+                            return Center(child: CircularProgressIndicator());
+                          }
+                        });
+                  } else {
+                    return Center(child: CircularProgressIndicator());
+                  }
+                },
+              ),
             );
           },
         ),
