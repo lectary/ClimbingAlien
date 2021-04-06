11b6a8fbfcaae4ab53b6284ad9823fa99941648e
Günter Windsperger <e1302775@student.tuwien.ac.at>
2021-04-07 01:09:11 +0200

    Add service class for storing files in app directory

---
+++ lib/services/storage_service.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+
+/// Service class for saving and removing images from local device storage.
+class StorageService {
+  static Future<String?> saveToDevice(String filePath) async {
+    File outFile = File(filePath);
+    String fileName = basename(filePath);
+
+    final directory = await getApplicationDocumentsDirectory();
+    String newPath = '${directory.path}/$fileName';
+
+    await outFile.copy(newPath);
+    return newPath;
+  }
+
+  static Future<void> deleteFromDevice(String filePath) async {
+    await File(filePath).delete();
+  }
+}
