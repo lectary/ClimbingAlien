f1eed021f4298c220aa02c61783a42bac14e06f4
Günter Windsperger <e1302775@student.tuwien.ac.at>
2021-04-15 12:13:28 +0200

    Integrate wall-image-downloader to routeScreen to trigger auto when needed

--- lib/views/route_management/route_screen.dart
+++ lib/views/route_management/route_screen.dart
@@ -3,24 +3,28 @@ import 'package:climbing_alien/data/entity/grasp.dart';
 import 'package:climbing_alien/data/entity/route.dart';
 import 'package:climbing_alien/data/entity/wall.dart';
 import 'package:climbing_alien/utils/dialogs.dart';
+import 'package:climbing_alien/viewmodels/wall_viewmodel.dart';
 import 'package:climbing_alien/views/route_editor/route_editor_screen.dart';
 import 'package:climbing_alien/views/route_management/route_form.dart';
 import 'package:climbing_alien/views/route_management/route_viewmodel.dart';
+import 'package:climbing_alien/views/wall_management/wall_image_downloader.dart';
 import 'package:flutter/material.dart' hide Route;
 import 'package:provider/provider.dart';
 
 class RouteScreen extends StatelessWidget {
-  final Wall wall;
-
-  RouteScreen(this.wall);
 
   @override
   Widget build(BuildContext context) {
     final climbingRepository = Provider.of<ClimbingRepository>(context, listen: false);
     return ChangeNotifierProvider(
-      create: (context) => RouteViewModel(climbingRepository: climbingRepository, wall: wall),
+      create: (context) => RouteViewModel(climbingRepository: climbingRepository, wall: Provider.of<WallViewModel>(context, listen: false).selectedWall),
       child: Consumer<RouteViewModel>(
-        builder: (context, routeModel, child) => Scaffold(
+        builder: (context, routeModel, child) {
+          final wall = context.select((WallViewModel model) => model.selectedWall!);
+          if (wall.status == WallStatus.persisted && wall.filePath == null) {
+            WidgetsBinding.instance?.addPostFrameCallback((_) => WallImageDownloader.asDialog(context, wall));
+          }
+          return Scaffold(
           appBar: AppBar(
             title: Text("${wall.title} - Routes"),
           ),
@@ -31,7 +35,7 @@ class RouteScreen extends StatelessWidget {
                   final routeList = snapshot.data!;
                   return routeList.isEmpty
                       ? Center(child: Text("No routes available"))
-                      : _buildList(context, routeList, routeModel);
+                      : _buildList(context, routeList, routeModel, wall);
                 } else {
                   return Center(child: CircularProgressIndicator());
                 }
@@ -40,62 +44,67 @@ class RouteScreen extends StatelessWidget {
             child: Icon(Icons.add),
             onPressed: () => RouteForm.showRouteFormDialog(context, wall),
           ),
-        ),
+        );
+        },
       ),
     );
   }
 
-  _buildList(BuildContext context, List<Route> routeList, RouteViewModel routeModel) {
-    return ListView.builder(
-        itemCount: routeList.length,
-        itemBuilder: (context, index) {
-          final route = routeList[index];
-          return ListTile(
-            title: Text(route.title),
-            subtitle: StreamBuilder<List<Grasp>>(
-                stream: routeModel.getGraspStreamByRouteId(route.id!),
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    final graspList = snapshot.data!;
-                    return Text("${route.description} - ${graspList.length} Grasps");
-                  } else {
-                    return Center(child: CircularProgressIndicator());
-                  }
-                }),
-            trailing: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                IconButton(
-                    icon: Icon(Icons.edit),
-                    onPressed: () => RouteForm.showRouteFormDialog(context, wall, route: route)),
-                IconButton(
-                    icon: Icon(Icons.delete),
-                    onPressed: () async => await Dialogs.showAlertDialog(
-                        context: context,
-                        title: 'Möchten Sie wirklich diese Route samt allen Griffen löschen?',
-                        submitText: 'Löschen',
-                        submitFunc: () async {
-                          bool isLastRoute = await routeModel.deleteRoute(route);
-                          // TODO re-query status of wall!
-                          if (isLastRoute && (wall.status == WallStatus.removed || wall.isCustom)) {
-                            await Dialogs.showAlertDialog(
-                                context: context,
-                                // TODO review usability
-                                title: wall.isCustom
-                                    ? 'Wenn Sie die letzte Route löschen, wird auch die Wand gelöscht!'
-                                    : 'Diese Wand ist am Server nicht mehr vorhanden! Wenn Sie die letzte Route löschen, wird die Wand nicht mehr verfügbar sein!',
-                                submitText: 'Löschen',
-                                submitFunc: () => routeModel.deleteRoute(route, forceDelete: true));
-                          } else {
-                            routeModel.deleteRoute(route, forceDelete: true);
-                          }
-                        }))
-              ],
-            ),
-            onLongPress: () => RouteForm.showRouteFormDialog(context, wall, route: route),
-            onTap: () =>
-                Navigator.push(context, MaterialPageRoute(builder: (context) => RouteEditorScreen(wall, route))),
-          );
-        });
+  _buildList(BuildContext context, List<Route> routeList, RouteViewModel routeModel, Wall wall) {
+    return Builder(
+      builder: (context) {
+        return ListView.builder(
+          itemCount: routeList.length,
+          itemBuilder: (context, index) {
+            final route = routeList[index];
+            return ListTile(
+              title: Text(route.title),
+              subtitle: StreamBuilder<List<Grasp>>(
+                  stream: routeModel.getGraspStreamByRouteId(route.id!),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      final graspList = snapshot.data!;
+                      return Text("${route.description} - ${graspList.length} Grasps");
+                    } else {
+                      return Center(child: CircularProgressIndicator());
+                    }
+                  }),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                      icon: Icon(Icons.edit),
+                      onPressed: () => RouteForm.showRouteFormDialog(context, wall, route: route)),
+                  IconButton(
+                      icon: Icon(Icons.delete),
+                      onPressed: () async => await Dialogs.showAlertDialog(
+                          context: context,
+                          title: 'Möchten Sie wirklich diese Route samt allen Griffen löschen?',
+                          submitText: 'Löschen',
+                          submitFunc: () async {
+                            bool isLastRoute = await routeModel.deleteRoute(route);
+                            // TODO re-query status of wall!
+                            if (isLastRoute && (wall.status == WallStatus.removed || wall.isCustom)) {
+                              await Dialogs.showAlertDialog(
+                                  context: context,
+                                  // TODO review usability
+                                  title: wall.isCustom
+                                      ? 'Wenn Sie die letzte Route löschen, wird auch die Wand gelöscht!'
+                                      : 'Diese Wand ist am Server nicht mehr vorhanden! Wenn Sie die letzte Route löschen, wird die Wand nicht mehr verfügbar sein!',
+                                  submitText: 'Löschen',
+                                  submitFunc: () => routeModel.deleteRoute(route, forceDelete: true));
+                            } else {
+                              routeModel.deleteRoute(route, forceDelete: true);
+                            }
+                          }))
+                ],
+              ),
+              onLongPress: () => RouteForm.showRouteFormDialog(context, wall, route: route),
+              onTap: () =>
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => RouteEditorScreen(wall, route))),
+            );
+          });
+      },
+    );
   }
 }
